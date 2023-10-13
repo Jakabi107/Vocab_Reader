4585e3864e1338fd44e07eb1cec7a32994969ce7
Jakabi107 <jakob.rietzler@gmail.com>
2023-10-13 22:49:27 +0200

    start with upload feature

--- vocab_reader/src/app/app.module.ts
+++ vocab_reader/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { UploadBoxComponent } from './upload-box/upload-box.component';
+import { UploadBoxRootComponent } from './upload-box-root/upload-box-root.component';
+import { AddFieldButtonComponent } from './add-field-button/add-field-button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UploadBoxComponent,
+    UploadBoxRootComponent,
+    AddFieldButtonComponent
   ],
   imports: [
     BrowserModule
